b1141bdc9c226667428b248aa9aa086eea766573
Lyashka <marzlyaha@mail.ru>
2024-05-07 12:48:37 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-app-bar>  
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+    <v-app-bar-title>Application Bar</v-app-bar-title>
+   
+    <template v-slot:append>
+      <v-btn icon="mdi-heart"></v-btn>
+  
+      <v-btn icon="mdi-magnify"></v-btn>
+  
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+    </template>
+  </v-app-bar>
+  
+  <v-main>
+    <RouterView />
+  </v-main>
+  
+  </v-app>  
+  
+      <!-- <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+  </template>
+
+<script setup> 
+import { RouterView } from 'vue-router'
+// import SideNavigationMenu from '@/components/SideNavigationMenu.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SelectDate.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="text-center mt-5">
+      <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        location="center"
+      >
+        <template v-slot:activator="{ props }">
+          <v-btn
+            prepend-icon="$calendar" 
+            color="white"
+            v-bind="props"
+          >  
+            {{ selectedDate.toLocaleDateString() }}
+          </v-btn>
+        </template>
+  
+        <v-card min-height="300px">
+          <v-date-picker v-model="selectedDate"></v-date-picker>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              variant="text" 
+              @click="menu = false"
+            >
+              OK
+            </v-btn> 
+          </v-card-actions>
+        </v-card>
+      </v-menu>
+    </div>
+  </template>
+  
+  <script setup> 
+  import {ref, watch} from 'vue'     
+
+  let menu = ref(false)
+  
+  let selectedDate = ref(new Date());
+
+
+
+watch(selectedDate, () => {
+ 
+    console.log(selectedDate.value);
+
+});
+
+
+  
+  </script>
+
+<style scoped lang="scss"></style>
